6dfd73e4219613b5a57759ca14fc886fcc1a0bb2
harchs <haroldrangelcharris@gmail.com>
2017-06-07 07:39:02 -0500

    CS-21 Filter businesses by text

--- src/actions/business.js
+++ src/actions/business.js
@@ -1,6 +1,8 @@
 import {BusinessTypes as types} from '../action-types';
 import httpRequest from '../services/httpRequest';
-import { browserHistory } from 'react-router'
+import { browserHistory } from 'react-router';
+import queryString from 'query-string';
+import _ from 'lodash';
 
 const businessesDataObject = businesses => {
   return {
@@ -16,5 +18,25 @@ export function fetchBusinesses(filters) {
     });
     const {businesses} = httpResponse.data;
     dispatch(businessesDataObject(businesses));
+  };
+}
+
+export function filterBusinessesByName(name, currentFilters) {
+  return async (dispatch: Function) => {
+    let filterByName = {};
+    filterByName['filters[name_cont]'] = name;
+    let filters = Object.assign({}, currentFilters, filterByName);
+    if (!name){
+      filters = _.omit(filters, 'filters[name_cont]');
+    }
+    const httpResponse = await httpRequest.get('/businesses', {
+      params: filters
+    });
+    const {businesses} = httpResponse.data;
+    dispatch(businessesDataObject(businesses));
+    browserHistory.push({
+      pathname: '/businesses',
+      search: `?${queryString.stringify(filters, {encode: false})}`
+    });
   };
 }
