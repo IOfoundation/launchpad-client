5a5cffe6ca8dc1a36ff675cf405950dacd94fc29
codypearce <codyapearce@gmail.com>
2017-06-09 18:47:32 -0600

    CS-80 style footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
 import {Link} from 'react-router';
+import Logo from './Logo';
 
 const Footer = () => {
   return (
-    <footer>
-      {'The footer'}
+    <footer className="row">
+      <div className="col-md-2">
+        <Logo />
+      </div>
+      <div className="col-md-2 col-md-offset-1">
+        <p className="footer_subtitle">{'FOOTER NAV'}</p>
+        <Link to="about">{'About'}</Link>
+        <Link to="about">{'Fermentum Ultricies'}</Link>
+        <Link to="about">{'Nullam'}</Link>
+      </div>
+      <div className="col-md-2">
+        <p className="footer_subtitle">{'FOOTER NAV'}</p>
+        <Link to="about">{'About'}</Link>
+        <Link to="about">{'Fermentum Ultricies'}</Link>
+        <Link to="about">{'Nullam'}</Link>
+      </div>
+      <div className="col-md-2">
+        <p className="footer_subtitle">{'FOOTER NAV'}</p>
+        <Link to="about">{'About'}</Link>
+        <Link to="about">{'Fermentum Ultricies'}</Link>
+        <Link to="about">{'Nullam'}</Link>
+      </div>
+      <div className="col-md-2 col-md-offset-1">
+        <p className="footer_subtitle">{'SOCIAL'}</p>
+        <Link to="twitter"><img src="" /></Link>
+        <Link to="twitter"><img src="" /></Link>
+        <Link to="twitter"><img src="" /></Link>
+      </div>
     </footer>
   );
 };
