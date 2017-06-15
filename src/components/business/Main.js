709ac3e4972df9c8df3855acbb64176f6ac5f34d
codypearce <codyapearce@gmail.com>
2017-06-15 13:27:37 -0600

    CS-89 business main toplogo and address styled

--- src/components/business/Main.js
+++ src/components/business/Main.js
@@ -3,16 +3,23 @@ import {PropTypes} from 'prop-types';
 import {browserHistory} from 'react-router';
 
 import MapView from '../map-view/Main';
+import {MdShare} from 'react-icons/lib/md';
 
 const Main = ({business}) => {
   const businesses = [business];
   return (
     <div className="">
-      <div>
+      <div className="row between-xs middle-xs">
         <span onClick={browserHistory.goBack}>{'Back To Resources'}</span>
+        <div>
+          <span>{'Share'}</span>
+          <MdShare size={22} color={'black'} />
+        </div>
       </div>
       <div className="">
-        <img src={business.log} alt={`${business.name} logo`} />
+        <div>
+          <img src={business.logo} alt={`${business.name} logo`} />
+        </div>
         <div>
           <h2>{business.name}</h2>
           <span>{'CONTACT'}</span>
@@ -25,12 +32,29 @@ const Main = ({business}) => {
             <li>{business.phone}</li>
             <li>{business.email}</li>
           </ul>
-          <ul>
-            <li><a href="#">{'VISIT WEBSITE'}</a></li>
-            <li><a href="#">{'Twitter'}</a></li>
-            <li><a href="#">{'Facebook'}</a></li>
-            <li><a href="#">{'LinkedIn'}</a></li>
-          </ul>
+          <section className="row business_block--reduced_bottom">
+            <a className="visitWebsite bold smallFont" href="#">
+              {'VISIT WEBSITE'}
+            </a>
+            <a className="socialIcon" href="#">
+              <img
+                className="socialIcon_icon"
+                src="static-data/images/twitter.svg"
+              />
+            </a>
+            <a className="socialIcon" href="#">
+              <img
+                className="socialIcon_icon"
+                src="static-data/images/facebook.svg"
+              />
+            </a>
+            <a className="socialIcon" href="#">
+              <img
+                className="socialIcon_icon"
+                src="static-data/images/linkedin.svg"
+              />
+            </a>
+          </section>
         </div>
         <div>
           <h3>{'SERVICE DESCRIPTION'}</h3>
@@ -39,9 +63,9 @@ const Main = ({business}) => {
         <div>
           <h3>{'SERVICES OFFERED'}</h3>
           <ul>
-            {business.Services.map(service => (
+            {business.Services.map(service =>
               <li key={service.id} className={'services'}>{service.name}</li>
-            ))}
+            )}
           </ul>
         </div>
       </div>
--- src/components/businesses/BusinessesList.js
+++ src/components/businesses/BusinessesList.js
@@ -3,9 +3,29 @@ import {PropTypes} from 'prop-types';
 
 import Business from './Business';
 
+const businessEx = {
+  name: 'Business name',
+  logo: 'static-data/images/test-logo.png',
+  address: '1456 Summerdale Rd',
+  city: 'corvallis',
+  country_code: '1',
+  zip_code: '59828',
+  phone: '555-5555-5555',
+  email: 'asdf@asdff.com',
+  description:
+    'Alphapointe is a diverse company that includes office supply stores, Call Center Services, plastics, office products, janitorial products, and textile manufacturing, and Low Vision Services.  It also offers more than 50,000 office supply products to businesses in the Kansas City area and nationally at www.alphapointeonline.com . Formed as a 501(c)(3) not-for-profit in 1911, Alphapointe is the largest employer of the blind in the state of Missouri.  It operates nine facilities in four states. The organization provides jobs and services to those who have lost sight.',
+  Services: ['service1'],
+  Stages: ['stages'],
+  Communities: ['Communities'],
+};
+
 const BusinessesList = ({businesses, handleClickOnBusiness, expanded}) => {
   return (
     <div>
+      <Business
+        business={businessEx}
+        handleClickOnBusiness={handleClickOnBusiness}
+      />
       {businesses.map(business => {
         return (
           <Business
