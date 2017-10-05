ee81cbdb9628d799e06fe381e2f8f0202500aa61
Camila Farelo <mfarelo92@gmail.com>
2017-10-05 10:08:25 -0500

    CS-190 filters should wrap

--- src/components/filters/FilterByText.js
+++ src/components/filters/FilterByText.js
@@ -41,36 +41,31 @@ class FilterByText extends React.Component {
           handleClick={this.deleteFilter.bind(this)}
         />
       );
-    } else {
-      return filters.category.map(filter => (
-        <Chip
-          key={filter}
-          text={filter}
-          handleClick={this.deleteFilter.bind(this)}
-        />
-      ));
     }
+    return filters.category.map(filter => (
+      <Chip
+        key={filter}
+        text={filter}
+        handleClick={this.deleteFilter.bind(this)}
+      />
+    ));
   }
   render() {
     const filters = this.props.getFilterChips();
     return (
       <div
         className={
-          this.state.labelTop ? (
-            'col-md-12 col-xs-12 text-xs-margin filterTextContainer noPadding m-bot-16'
-          ) : (
-            'col-md-12 col-xs-12 text-xs-margin filterTextContainer noPadding'
-          )
+          this.state.labelTop
+            ? 'col-md-12 col-xs-12 text-xs-margin filterTextContainer noPadding m-bot-16'
+            : 'col-md-12 col-xs-12 text-xs-margin filterTextContainer noPadding'
         }
       >
         <div className="grid search-text-form p-bot-16">
           <div
             className={
-              isEmpty(filters) ? (
-                'filter-label-container-hide'
-              ) : (
-                'filter-label-container-show'
-              )
+              isEmpty(filters) || this.state.labelTop
+                ? 'filter-label-container-hide'
+                : 'filter-label-container-show col-lg-9 noPadding'
             }
           >
             {this.renderFilter()}
@@ -80,44 +75,46 @@ class FilterByText extends React.Component {
           </div>
           <h3
             className={
-              isEmpty(filters) ? (
-                'show-filter col-lg-9 p-left-0'
-              ) : (
-                'col-lg-9 hide-filter'
-              )
+              isEmpty(filters)
+                ? 'show-filter col-lg-9 p-left-0'
+                : 'col-lg-9 hide-filter'
             }
           >
             {'Filter results with the selections below'}
           </h3>
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={event => this.handleChange(event)}
-            onClick={() => this._inputClicked()}
-            onKeyPress={target => this.handleKeyPress(target)}
-            placeholder={
-              this.state.labelTop ? (
-                'Search by Resource Name or Ipsum'
-              ) : (
-                'Or search by name'
-              )
-            }
+          <div
             className={
-              this.state.labelTop ? (
-                'full-width-search'
-              ) : (
-                'text-search small-width-search col-md-3'
-              )
+              this.state.labelTop
+                ? 'full-width-text-filter'
+                : 'col-lg-3 noPadding'
             }
-          />
-          {this.state.labelTop && (
-            <MdClear
-              className="text-search-icon"
-              size="32"
-              color="#2AD587"
-              onClick={() => this._closeSearch()}
+          >
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={event => this.handleChange(event)}
+              onClick={() => this._inputClicked()}
+              onKeyPress={target => this.handleKeyPress(target)}
+              placeholder={
+                this.state.labelTop
+                  ? 'Search by Resource Name or Ipsum'
+                  : 'Or search by name'
+              }
+              className={
+                this.state.labelTop
+                  ? 'full-width-search'
+                  : 'text-search small-width-search'
+              }
             />
-          )}
+            {this.state.labelTop && (
+              <MdClear
+                className="text-search-icon"
+                size="32"
+                color="#2AD587"
+                onClick={() => this._closeSearch()}
+              />
+            )}
+          </div>
           <div
             className={
               this.state.labelTop ? 'option-dropdown-show' : 'hero_input-hide'
