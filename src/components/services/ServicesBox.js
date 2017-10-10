4bb06a0ce22a98245f0a0b7d76c60a2f394fc449
Kate Piette <kat.piette@gmail.com>
2017-10-10 13:56:59 -0400

    CS-204 fix lint errors

---
+++ src/components/services/ServicesBox.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {PropTypes} from 'prop-types';
+import Service from './Service';
+
+const ServicesBox = ({services, handleClickOnServiceTag}) => {
+  return (
+    <div>
+      {services.map(service => (
+        <Service
+          key={service.id}
+          service={service}
+          handleClickOnServiceTag={handleClickOnServiceTag}
+        />
+      ))}
+    </div>
+  );
+};
+
+ServicesBox.propTypes = {
+  handleClickOnServiceTag: PropTypes.func.isRequired,
+  services: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default ServicesBox;
