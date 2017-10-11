dd4f8f1f1c03b82a8b46a3e739bf0bc785d6e7ef
Mike Piccolo <mfpiccolo@gmail.com>
2017-10-10 22:00:05 -0700

    Change key handlers

--- src/components/home/BusinessesForm.js
+++ src/components/home/BusinessesForm.js
@@ -10,15 +10,6 @@ class BusinessesForm extends React.Component {
       value: '',
       showDropdown: false,
     };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-  showDropdown() {
-    this.setState({showDropdown: !this.state.showDropdown});
   }
 
   handleClickOutside() {
@@ -26,7 +17,7 @@ class BusinessesForm extends React.Component {
   }
 
   handleKeyPress(value) {
-    this.showDropdown();
+    this.setState({value: event.target.value, showDropdown: true});
     this.props.handleTextSearchBusinessesForm(value);
   }
 
@@ -50,19 +41,16 @@ class BusinessesForm extends React.Component {
         <input
           type="text"
           value={this.state.value}
-          onChange={this.handleChange}
-          onKeyPress={e => this.handleKeyPress(e.target.value)}
+          onKeyUp={e => this.handleKeyPress(e.target.value)}
           placeholder="Search for businesses and services"
           className="hero_input businessesName"
         />
         <MdSearch className="text-search-icon" size={40} color={'#2AD587'} />
         <div
           className={
-            this.state.showDropdown ? (
-              'hero_input-dropdown hero_input-show'
-            ) : (
-              'hero_input-dropdown hero_input-hide'
-            )
+            this.state.showDropdown
+              ? 'hero_input-dropdown hero_input-show'
+              : 'hero_input-dropdown hero_input-hide'
           }
         >
           {this.renderDropdown()}
