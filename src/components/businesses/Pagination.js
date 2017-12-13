942ce403ce7c24072499f3509457712e295e0d0b
Kate Piette <kat.piette@gmail.com>
2017-12-13 14:27:47 -0500

    CS-463 remove size props from pagination

--- src/components/businesses/Pagination.js
+++ src/components/businesses/Pagination.js
@@ -57,7 +57,6 @@ class Pagination extends React.Component {
 Pagination.propTypes = {
   handleChangePage: PropTypes.func.isRequired,
   metadata: PropTypes.object.isRequired,
-  size: PropTypes.string,
 };
 
 export default Pagination;
