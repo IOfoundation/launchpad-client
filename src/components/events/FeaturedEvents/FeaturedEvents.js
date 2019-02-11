984e07a40a6952120c11dfacd4a12185fc9d467d
Cristian Marquez <cristian@fullstacklabs.co>
2019-02-11 17:14:48 -0500

    CS-587 Added reponsive layout

--- src/components/events/FeaturedEvents/FeaturedEvents.js
+++ src/components/events/FeaturedEvents/FeaturedEvents.js
@@ -1,60 +1,81 @@
 import React from 'react';
+import {containerStyles} from '../../../utils/containerStyles';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 import FeaturedEvent from './FeaturedEvent';
 
-const FeaturedEvents = () => {
+const styles = theme => ({
+  content: {
+    padding: 12,
+    ...containerStyles(theme),
+  },
+});
+
+const FeaturedEvents = props => {
+  const {classes} = props;
+
   return (
-    <div className="featured-events">
-      <div className="featured-events__row">
-        <h2 className="featured-events__title text-bold">{'November'}</h2>
-        <FeaturedEvent
-          title="Special Conference for Lorem"
-          description="Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-          date="November 2, 2017"
-          name="Resource Name"
-        />
+    <div className={[classes.content, 'featured-events'].join(' ')}>
+      <Grid container={true} spacing={24}>
+        <Grid item={true} xs={4}>
+          <h2 className="featured-events__title text-bold">{'November'}</h2>
+          <FeaturedEvent
+            title="Special Conference for Lorem"
+            description="Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+            date="November 2, 2017"
+            name="Resource Name"
+          />
 
-        <FeaturedEvent
-          title="Raising Investor Capital The Right Way"
-          description="Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros"
-          date="November 2, 2017"
-          name="Resource Name"
-        />
-      </div>
-      <div className="featured-events__row">
-        <h2 className="featured-events__title text-bold">{'Dicember'}</h2>
-        <FeaturedEvent
-          title="Raising Investor Capital The Right Way"
-          description="Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros"
-          date="November 12, 2017"
-          name="Resource Name"
-        />
+          <FeaturedEvent
+            title="Raising Investor Capital The Right Way"
+            description="Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros"
+            date="November 2, 2017"
+            name="Resource Name"
+          />
+        </Grid>
+        <Grid item={true} xs={4}>
+          <h2 className="featured-events__title text-bold">{'Dicember'}</h2>
+          <FeaturedEvent
+            title="Raising Investor Capital The Right Way"
+            description="Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros"
+            date="November 12, 2017"
+            name="Resource Name"
+          />
 
-        <FeaturedEvent
-          title="Special Conference for Long Name Ipsum"
-          description="Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
-          date="November 2, 2017"
-          name="Resource Name"
-        />
+          <FeaturedEvent
+            title="Special Conference for Long Name Ipsum"
+            description="Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+            date="November 2, 2017"
+            name="Resource Name"
+          />
 
-        <FeaturedEvent
-          title="Managing Your Books"
-          description="Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros"
-          date="November 12, 2017"
-          name="Resource Name"
-        />
-      </div>
-      <div className="featured-events__row">
-        <h2 className="featured-events__title text-bold">{'January'}</h2>
-        <FeaturedEvent
-          title="Managing Your Books"
-          description="Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros"
-          date="November 12, 2017"
-          name="Resource Name"
-        />
-      </div>
+          <FeaturedEvent
+            title="Managing Your Books"
+            description="Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros"
+            date="November 12, 2017"
+            name="Resource Name"
+          />
+        </Grid>
+        <Grid item={true} xs={4}>
+          <h2 className="featured-events__title text-bold">{'January'}</h2>
+          <FeaturedEvent
+            title="Managing Your Books"
+            description="Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros"
+            date="November 12, 2017"
+            name="Resource Name"
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
-export default FeaturedEvents;
+FeaturedEvents.propTypes = {
+  classes: PropTypes.shape({
+    content: PropTypes.string,
+  }),
+};
+
+export default withStyles(styles)(FeaturedEvents);
