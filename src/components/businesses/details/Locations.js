f40e2da048e422b749a80f72b9b8f175aa67ef88
Cristian Marquez <cristian@fullstacklabs.co>
2019-02-12 11:56:40 -0500

    CS-582 Added modal to location and some styles fixes

--- src/components/businesses/details/Locations.js
+++ src/components/businesses/details/Locations.js
@@ -1,68 +1,110 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
+import {withStyles} from '@material-ui/core/styles';
 import {Link} from 'react-router';
 import Location from './Location';
 import {PropTypes} from 'prop-types';
+import Modal from '@material-ui/core/Modal';
+import LocationDetails from './LocastionDetails';
 
-const Locations = props => {
-  const {locations} = props;
-  let $otherLocation = null;
+const styles = theme => ({
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    left: '50%',
+    outline: 'none',
+    padding: theme.spacing.unit * 4,
+    position: 'absolute',
+    top: '40%',
+    transform: 'translate(-50%, -50%)',
+    width: theme.spacing.unit * 70,
+  },
+});
 
-  if (locations.length > 1) {
-    const mapOtherLocations = locations.map((location, index) => {
-      if (index === 0) {
+class Locations extends PureComponent {
+  state = {
+    open: false,
+    organizationSelected: this.props.locations[0],
+  };
+
+  detailsClickedHandler = organizationSelected => {
+    this.setState({open: true, organizationSelected});
+  };
+
+  closeModalHandler = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const {locations, classes} = this.props;
+    const {organizationSelected} = this.state;
+
+    let $otherLocation = null;
+
+    if (locations.length > 1) {
+      const mapOtherLocations = locations.map((location, index) => {
+        if (index === 0) {
+          return (
+            <h2 key={1} className="detail-locations__title text-bold">
+              {'Other Location'}
+            </h2>
+          );
+        }
+
         return (
-          <h2 key={1} className="detail-locations__title text-bold">
-            {'Other Location'}
-          </h2>
+          <Location
+            address={`${location.address.address_1}, ${
+              location.address.address_2
+            }, ${location.address.state_province} ${
+              location.address.postal_code
+            }`}
+            title={location.address.city}
+            email="email@domaninname.com"
+            phone="(916) 514-7044"
+            key={location.id}
+            onDetailsClicked={() => this.detailsClickedHandler(location)}
+          />
         );
-      }
+      });
 
-      return (
+      $otherLocation = [
+        ...mapOtherLocations,
+        <Link key={'link'} className="detail-locations__all-locations">
+          {'View All Locations'}
+        </Link>,
+      ];
+    }
+
+    return (
+      <div className="detail-locations">
+        <Modal open={this.state.open} onClose={this.closeModalHandler}>
+          <div className={classes.paper}>
+            <LocationDetails organization={organizationSelected} />
+          </div>
+        </Modal>
+        <h2 className="detail-locations__title text-bold">{'Main Location'}</h2>
         <Location
-          address={`${location.address.address_1}, ${
-            location.address.address_2
-          }, ${location.address.state_province} ${
-            location.address.postal_code
+          address={`${locations[0].address.address_1}, ${
+            locations[0].address.address_2
+          }, ${locations[0].address.state_province} ${
+            locations[0].address.postal_code
           }`}
-          title={location.address.city}
+          title={locations[0].address.city}
           email="email@domaninname.com"
           phone="(916) 514-7044"
-          key={location.id}
+          onDetailsClicked={() => this.detailsClickedHandler(locations[0])}
         />
-      );
-    });
-
-    $otherLocation = [
-      ...mapOtherLocations,
-      <Link key={'link'} className="detail-locations__all-locations">
-        {'View All Locations'}
-      </Link>,
-    ];
+        {$otherLocation}
+      </div>
+    );
   }
-
-  return (
-    <div className="detail-locations">
-      <h2 className="detail-locations__title text-bold">{'Main Location'}</h2>
-      <Location
-        address={`${locations[0].address.address_1}, ${
-          locations[0].address.address_2
-        }, ${locations[0].address.state_province} ${
-          locations[0].address.postal_code
-        }`}
-        title={locations[0].address.city}
-        email="email@domaninname.com"
-        phone="(916) 514-7044"
-      />
-      {$otherLocation}
-    </div>
-  );
-};
+}
 
 Locations.propTypes = {
+  classes: PropTypes.shape({
+    paper: PropTypes.string,
+  }),
   locations: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
       address: PropTypes.shape({
         address_1: PropTypes.string,
         address_2: PropTypes.string,
@@ -70,10 +112,12 @@ Locations.propTypes = {
         postal_code: PropTypes.string,
         state_province: PropTypes.string,
       }),
-      phones: PropTypes.array,
       email: PropTypes.string,
+      id: PropTypes.number,
+      name: PropTypes.string,
+      phones: PropTypes.array,
     })
   ),
 };
 
-export default Locations;
+export default withStyles(styles)(Locations);
