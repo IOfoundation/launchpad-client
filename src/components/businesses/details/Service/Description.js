fd19f0b7c4d5f25455ba63813606fba3c7703f19
Cristian Marquez <cristian@fullstacklabs.co>
2019-02-15 10:51:57 -0500

    CS-580 Added viewmore on description

---
+++ src/components/businesses/details/Service/Description.js
@@ -0,0 +1,49 @@
+import React, {PureComponent} from 'react';
+import {PropTypes} from 'prop-types';
+import {truncate} from '../../../../utils';
+
+class Description extends PureComponent {
+  state = {
+    viewMore: false,
+  };
+
+  viewMoreHandler = () => {
+    this.setState(prevState => {
+      return {
+        viewMore: !prevState.viewMore,
+      };
+    });
+  };
+
+  render() {
+    const {description} = this.props;
+    const {viewMore} = this.state;
+    let $viewMore = (
+      <p className="business-details-section__information__content">
+        {description}
+      </p>
+    );
+
+    if (description.split(' ').length > 50) {
+      $viewMore = (
+        <p className="business-details-section__information__content">
+          {viewMore ? description : truncate(description, 50, '...')}
+          <a
+            className="service__view-more text-semi"
+            onClick={this.viewMoreHandler}
+          >
+            {viewMore ? 'View Less' : 'View More'}
+          </a>
+        </p>
+      );
+    }
+
+    return $viewMore;
+  }
+}
+
+Description.propTypes = {
+  description: PropTypes.string,
+};
+
+export default Description;
