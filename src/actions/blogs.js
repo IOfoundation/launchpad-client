aa077036126cc8842bd7da6dbb910405ae313f54
Cristian Marquez <cristian@fullstacklabs.co>
2019-02-27 11:46:51 -0500

    CS-597 Added featured posts to the blog page

--- src/actions/blogs.js
+++ src/actions/blogs.js
@@ -8,16 +8,37 @@ const getFeaturedPostsStarts = config => {
   };
 };
 
-const getFeaturedPostsSuccess = organizationPosts => {
+const getFeaturedPostsSuccess = featuredPosts => {
   return {
     type: types.GET_FEATURED_POSTS_SUCCESS,
-    organizationPosts,
+    featuredPosts,
   };
 };
 
 const getFeaturedPostsError = error => {
   return {
     type: types.GET_FEATURED_POSTS_ERROR,
+    error,
+  };
+};
+
+const getFeaturedPostsByIdStart = config => {
+  return {
+    type: types.GET_FEATURED_POSTS_BY_ID_START,
+    ...config,
+  };
+};
+
+const getFeaturedPostsByIdSuccess = organizationPosts => {
+  return {
+    type: types.GET_FEATURED_POSTS_BY_ID_SUCCESS,
+    organizationPosts,
+  };
+};
+
+const getFeaturedPostsByIdError = error => {
+  return {
+    type: types.GET_FEATURED_POSTS_BY_ID_ERROR,
     error,
   };
 };
@@ -32,9 +53,26 @@ export const noResults = value => {
 export const getFeaturedPostById = id => {
   return async dispatch => {
     try {
-      dispatch(getFeaturedPostsStarts());
+      dispatch(getFeaturedPostsByIdStart());
       const httpResponse = await httpRequest.get(
         `/api/blog_posts?filter[organization_id]=${id}`
+      );
+      if (httpResponse.data.length === 0) {
+        dispatch(noResults(true));
+      }
+      dispatch(getFeaturedPostsByIdSuccess(httpResponse.data));
+    } catch (error) {
+      dispatch(getFeaturedPostsByIdError(error));
+    }
+  };
+};
+
+export const getFeaturedPost = () => {
+  return async dispatch => {
+    try {
+      dispatch(getFeaturedPostsStarts());
+      const httpResponse = await httpRequest.get(
+        '/api/blog_posts?filter[category]=featured'
       );
       if (httpResponse.data.length === 0) {
         dispatch(noResults(true));
