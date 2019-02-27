5f888c593f940e9f6d31ddb035e4667e4b8b0dbd
Cristian Marquez <cristian@fullstacklabs.co>
2019-02-27 13:11:13 -0500

    CS-598 Display "Front Page" posts

--- src/actions/blogs.js
+++ src/actions/blogs.js
@@ -1,5 +1,6 @@
 import {BlogsTypes as types} from '../action-types';
 import {httpRequest} from '../utils';
+import {getDate} from '../utils';
 
 const getFeaturedPostsStarts = config => {
   return {
@@ -43,6 +44,27 @@ const getFeaturedPostsByIdError = error => {
   };
 };
 
+const getAllPostsStarts = config => {
+  return {
+    type: types.GET_ALL_POSTS_START,
+    ...config,
+  };
+};
+
+const getAllPostsSuccess = posts => {
+  return {
+    type: types.GET_ALL_POSTS_SUCCESS,
+    posts,
+  };
+};
+
+const getAllPostsError = error => {
+  return {
+    type: types.GET_ALL_POSTS_ERROR,
+    error,
+  };
+};
+
 export const noResults = value => {
   return {
     type: types.NO_RESULTS,
@@ -80,6 +102,30 @@ export const getFeaturedPost = () => {
       dispatch(getFeaturedPostsSuccess(httpResponse.data));
     } catch (error) {
       dispatch(getFeaturedPostsError(error));
+    }
+  };
+};
+
+export const getAllPosts = page => {
+  return async dispatch => {
+    try {
+      dispatch(getAllPostsStarts());
+      const httpResponse = await httpRequest.get(
+        `/api/blog_posts?page=${page}&per_page=5&filter[category]=${'front page'}`
+      );
+      if (httpResponse.data.length === 0) {
+        dispatch(noResults(true));
+      }
+
+      const formatedData = httpResponse.data.map(data => {
+        data.posted_at = getDate(data.posted_at);
+
+        return data;
+      });
+
+      dispatch(getAllPostsSuccess(formatedData));
+    } catch (error) {
+      dispatch(getAllPostsError(error));
     }
   };
 };
--- src/components/blog/PostLists.js
+++ src/components/blog/PostLists.js
@@ -1,49 +1,89 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import {PropTypes} from 'prop-types';
+import {truncate, maxCharacters} from '../../utils';
+
 import PostListItem from './PostListItem';
+import * as actions from '../../actions/blogs';
+import Loading from '../shared/Loading';
 
-const PostLists = props => {
-  const {section} = props;
+class PostLists extends PureComponent {
+  state = {
+    page: 1,
+  };
 
-  return (
-    <div className="blog-posts">
-      <h2 className="blog-posts__title">{section}</h2>
-      <PostListItem
-        title="Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua Eni Sunt Proident Blandit Aute In Dolor"
-        description="Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Lorem ipsum dolor. Tempus curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Lorem maecenas faucibus."
-        tag="Governor’s Office of Business and Economic Development"
-        date="February 2, 2018"
-      />
-      <PostListItem
-        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur."
-        description="Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes..."
-        tag="Grow America Fund"
-        date="February 2, 2018"
-      />
-      <PostListItem
-        title="Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua Eni Sunt Proident Blandit Aute In Dolor"
-        description="Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Lorem ipsum dolor. Tempus curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Lorem maecenas faucibus."
-        tag="Governor’s Office of Business and Economic Development"
-        date="February 2, 2018"
-      />
-      <PostListItem
-        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur."
-        description="Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes."
-        tag="Grow America Fund"
-        date="February 2, 2018"
-      />
-      <PostListItem
-        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur."
-        description="Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes."
-        tag="Grow America Fund"
-        date="February 2, 2018"
-      />
-    </div>
-  );
+  componentDidMount() {
+    this.props.actions.getAllPosts(this.state.page);
+  }
+
+  render() {
+    const {section, posts} = this.props;
+    let resultsElements = (
+      <Loading elementConfig={{style: {margin: '0 auto', padding: 0}}} />
+    );
+
+    if (posts.results.length > 0) {
+      resultsElements = posts.results.map(post => {
+        const date = post.posted_at;
+        let description = post.body;
+        let title = post.title;
+
+        if (description.split('').length > maxCharacters) {
+          description = truncate(description);
+        }
+
+        if (title.split('').length > 120) {
+          title = truncate(title, 110);
+        }
+
+        return (
+          <PostListItem
+            key={post.id}
+            title={title}
+            description={description}
+            tag={post.organization.name}
+            date={`${date.monthLarge} ${date.day}, ${date.year}`}
+          />
+        );
+      });
+    }
+
+    return (
+      <div className="blog-posts">
+        <h2 className="blog-posts__title">{section}</h2>
+        {resultsElements}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = _state => {
+  const {blogs: _blogs} = _state;
+
+  return {
+    posts: {results: _blogs.posts, noResults: _blogs.noResults},
+  };
+};
+
+const mapDispatchToProps = _dispatch => {
+  return {
+    actions: bindActionCreators(actions, _dispatch),
+  };
 };
 
 PostLists.propTypes = {
+  actions: PropTypes.shape({
+    getAllPosts: PropTypes.func,
+  }),
+  posts: PropTypes.shape({
+    results: PropTypes.arrayOf(PropTypes.shape({})),
+    noResults: PropTypes.bool,
+  }),
   section: PropTypes.string,
 };
 
-export default PostLists;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PostLists);
