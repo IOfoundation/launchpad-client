7f5675f8d18a547a9f74f482591dbca40fa9296f
Cristian Marquez <cristian@fullstacklabs.co>
2019-03-25 21:26:08 -0400

    CS-637 Prepare data to update on the backend

--- src/components/admin-site/elements/LocationFormContainer.js
+++ src/components/admin-site/elements/LocationFormContainer.js
@@ -143,8 +143,11 @@ const initialValues = {
 };
 
 class LocationFormContainer extends PureComponent {
-  _getAddres(address) {
+  _submit;
+  _apiData;
+  _getAddress(address) {
     return {
+      id: address.id,
       address: (address && address.address_1) || '',
       address2: (address && address.address_2) || '',
       city: (address && address.city) || '',
@@ -154,15 +157,40 @@ class LocationFormContainer extends PureComponent {
     };
   }
 
+  _getApiAddress(address) {
+    return {
+      id: address.id,
+      address_1: address.address,
+      address_2: address.address2,
+      city: address.city,
+      state_province: address.state,
+      postal_code: address.zip,
+    };
+  }
+
   _getHours(schedule) {
     return schedule.map(hour => {
       const open = getDate(hour.opens_at);
       const closes = getDate(hour.closes_at);
 
       return {
+        id: hour.id,
         closesAt: timeConversion(closes.time),
+        closes,
         day: 'Option One',
         opensAt: timeConversion(open.time),
+        open,
+      };
+    });
+  }
+
+  _getApiHours(schedule) {
+    return schedule.map(hour => {
+      return {
+        id: hour.id,
+        weekday: hour.open.day,
+        opens_at: hour.open.fullDate,
+        closes_at: hour.closes.fullDate,
       };
     });
   }
@@ -202,6 +230,17 @@ class LocationFormContainer extends PureComponent {
     });
   }
 
+  _getApiPhones(phones) {
+    return phones.map(phone => ({
+      id: phone.id,
+      department: phone.department,
+      extension: phone.ext,
+      number: phone.phoneNumber,
+      number_type: phone.numberType,
+      vanity_number: phone.vanityNumber,
+    }));
+  }
+
   _getAccessibility(options) {
     return options.reduce((acc, option) => {
       const activeAccesibility = accesibility.find(
@@ -212,7 +251,19 @@ class LocationFormContainer extends PureComponent {
     }, initialValues.accessibility);
   }
 
+  _getAccessibilityNames(checked) {
+    return Object.keys(checked).reduce((acc, key) => {
+      if (checked[key]) {
+        const activeAccesibility = accesibility.find(data => key === data.key);
+        acc.push(activeAccesibility.value);
+      }
+
+      return acc;
+    }, []);
+  }
+
   _dataToForm(data) {
+    this._apiData = data;
     return {
       ...initialValues,
       locationName: data.name || '',
@@ -220,8 +271,8 @@ class LocationFormContainer extends PureComponent {
       locationDescription: data.description || '',
       locationEmail: data.email || '',
       locationWebsite: data.website || '',
-      streetAddress: this._getAddres(data.address),
-      mailingAddress: this._getAddres(data.mail_address),
+      streetAddress: this._getAddress(data.address),
+      mailingAddress: this._getAddress(data.mail_address),
       phones: this._getPhones(data.phones),
       hoursRegular: this._getHours(data.regular_schedules),
       hoursHolidays: this._getHours(data.holiday_schedules),
@@ -239,6 +290,40 @@ class LocationFormContainer extends PureComponent {
     this.props.router.push('/admin/location');
   };
 
+  _formToApi = data => {
+    return {
+      id: this._apiData.id,
+      active: this._apiData.active,
+      admin_emails: this._apiData.admin_emails,
+      alternate_name: data.alternateName,
+      coordinates: this._apiData.coordinates,
+      description: data.description,
+      latitude: this._apiData.latitude,
+      longitude: this._apiData.longitude,
+      name: data.name,
+      short_desc: this._apiData.short_desc,
+      slug: this._apiData.slug,
+      website: data.locationWebsite,
+      updated_at: getDate().fullDate,
+      url: this._apiData.url,
+      accessibility: this._getAccessibilityNames(data.accessibility),
+      email: data.locationEmail,
+      languages: data.languages,
+      transportation: data.transportation,
+      address: this._getApiAddress(data.streetAddress),
+      organization: this._apiData.organization,
+      phones: this._getApiPhones(data.phones),
+      contacts: this._apiData.contacts,
+      mail_address: this._getApiAddress(data.mailingAddress),
+      regular_schedules: this._getApiHours(data.hoursRegular),
+      holiday_schedules: this._getApiHours(data.hoursHolidays),
+    };
+  };
+
+  saveLocation = () => {
+    this._submit();
+  };
+
   render() {
     const {data, breakpoint} = this.props;
 
@@ -249,14 +334,21 @@ class LocationFormContainer extends PureComponent {
           hideCancelAction={false}
           submitLabel={'Save location'}
           titleText="Create A Location"
+          cancelClicked={this.goToLocation}
+          submitClicked={this.saveLocation}
         />
         <Formik
           enableReinitialize={true}
           initialValues={this._dataToForm(data)}
-          onSubmit={() => {}}
-          render={_props => (
-            <LocationForm {..._props} breakpoint={breakpoint} />
-          )}
+          onSubmit={values => {
+            const form = this._formToApi(values);
+            console.log(values);
+            console.log(form);
+          }}
+          render={_props => {
+            this._submit = _props.handleSubmit;
+            return <LocationForm {..._props} breakpoint={breakpoint} />;
+          }}
           validateOnChange={true}
           validationSchema={LocationSchema}
         />
