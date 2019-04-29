29d35208e6c272a8c748769af455ff25a9b03c81
Cristian Marquez <cristian@fullstacklabs.co>
2019-04-29 14:18:17 -0400

    CS-765 Updated deploy server

--- deploy/index.js
+++ deploy/index.js
@@ -1,10 +1,14 @@
-var path = require('path');
-var express = require('express');
-var app = express();
+const path = require('path');
+const express = require('express');
+const app = express();
 
 app.use(express.static(path.resolve('./dist')));
-app.get('/*', function(req, res){
-  res.sendFile(path.resolve('./dist/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve('./dist/index.html'), err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3001);
